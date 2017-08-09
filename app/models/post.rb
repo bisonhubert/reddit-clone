54ef4259c65c811764de938a9982c547d5bb7c3d
Bison Hubert <davidbisonhubert@gmail.com>
2017-08-09 14:17:55 -0700

    Add comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,8 @@
 class Post < ApplicationRecord
   belongs_to :user
+  has_many :comments, dependent: :destroy
   default_scope -> { order(created_at: :desc) }
+  validates_presence_of :content, :title, :user_id
   validates :title, presence: true,
                     length: { minimum: 5 }
-  validates :content, presence: true
-  validates :user_id, presence: true
 end
---
+++ spec/factories/comments.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :comment do
+    content "MyText"
+    article nil
+    user nil
+  end
+end
