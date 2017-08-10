02bb15ca4ae327ec5226fa26e8038efa3c106d30
Bison Hubert <davidbisonhubert@gmail.com>
2017-08-09 18:41:28 -0700

    Style flash messages

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,16 +6,18 @@ class SessionsController < ApplicationController
     @user = User.find_by(email: params[:session][:email].downcase)
     if @user && @user.authenticate(params[:session][:password])
       log_in @user
+      flash[:success] = 'You are now logged in.'
       params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
       redirect_back_or @user
     else
-      flash.now[:danger] = 'Invalid email/password combination'
+      flash.now[:alert] = 'Invalid email/password combination.'
       render 'new'
     end
   end
 
   def destroy
     log_out if logged_in?
+    flash[:success] = 'You are now logged out.'
     redirect_to root_url
   end
 end
